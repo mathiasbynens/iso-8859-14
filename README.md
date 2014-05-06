adf0753c701e414546a45c0f9914f82ce56cbe07
Mathias Bynens <mathias@qiwi.be>
2014-05-06 18:11:11 +0200

    Expose encoding labels

--- README.md
+++ README.md
@@ -64,6 +64,10 @@ require(
 
 A string representing the semantic version number.
 
+### `iso885914.labels`
+
+An array of strings, each representing a [label](http://encoding.spec.whatwg.org/#label) for this encoding.
+
 ### `iso885914.encode(input, options)`
 
 This function takes a plain text string (the `input` parameter) and encodes it according to iso-8859-14. The return value is a ‘byte string’, i.e. a string of which each item represents an octet as per iso-8859-14.
--- iso885914.js
+++ iso885914.js
@@ -50,6 +50,7 @@
 		var length = input.length;
 		var index = -1;
 		var byteValue;
+		var pointer;
 		var result = '';
 		while (++index < length) {
 			byteValue = input.charCodeAt(index);
@@ -61,7 +62,7 @@
 			}
 			// “Let `code point` be the index code point for `byte − 0x80` in index
 			// `single-byte`.”
-			var pointer = byteValue - 0x80;
+			pointer = byteValue - 0x80;
 			if (hasOwnProperty.call(INDEX_BY_POINTER, pointer)) {
 				// “Return a code point whose value is `code point`.”
 				result += INDEX_BY_POINTER[pointer];
@@ -114,6 +115,11 @@
 	var iso885914 = {
 		'encode': encode,
 		'decode': decode,
+		'labels': [
+			'iso-8859-14',
+			'iso8859-14',
+			'iso885914'
+		],
 		'version': '0.1.0'
 	};
 
